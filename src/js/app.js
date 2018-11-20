1402753b548acbe9145a2b6c3200c22575ba1e7f
Yarden Rotem <sdada@sadas>
2018-11-20 15:15:52 +0200

    final and working perfectly

--- src/js/app.js
+++ src/js/app.js
@@ -6,54 +6,49 @@ $(document).ready(function () {
     $('#codeSubmissionButton').click(() => {
         let codeToParse = $('#codePlaceholder').val();
         let parsedCode = parseCode(codeToParse);
-        //console.log(parsedCode);
         var result = ParseDataToTable(parsedCode);
-        console.log(result);
         CreateTableFromJson(result);
         $('#parsedCode').val(JSON.stringify(parsedCode, null, 2));
     });
 });
 
 
+function AddNewlyTableWithJson(table) {
+    var divContainer = document.getElementById('showData');
+    divContainer.innerHTML = '';
+    divContainer.appendChild(table);
+
+}
+
+function CreateTheCols(myBooks) {
+    let col = [];
+    for (let i = 0; i < myBooks.length; i++) {
+        for (let key in myBooks[i]) {
+            if (col.indexOf(key) === -1) {
+                col.push(key);
+            }
+        }
+    }
+    return col;
+}
 
 function CreateTableFromJson(myBooks)
 {
-    var col = [];
-        for (var i = 0; i < myBooks.length; i++) {
-            for (var key in myBooks[i]) {
-                if (col.indexOf(key) === -1) {
-                    col.push(key);
-                }
-            }
+    var col = CreateTheCols(myBooks);
+    var table = document.createElement('table');
+    table.id='table2';
+    var tr = table.insertRow(-1);                   // TABLE ROW.
+    for (var i = 0; i < col.length; i++) {
+        var th = document.createElement('th');      // TABLE HEADER.
+        th.innerHTML = col[i];
+        tr.appendChild(th);
+    }
+    for (i = 0; i < myBooks.length; i++) {
+        tr = table.insertRow(-1);
+        for (var j = 0; j < col.length; j++) {
+            var tabCell = tr.insertCell(-1);
+            tabCell.innerHTML = myBooks[i][col[j]];
         }
-
-        // CREATE DYNAMIC TABLE.
-        var table = document.createElement("table");
-        table.id="table2";
-
-        // CREATE HTML TABLE HEADER ROW USING THE EXTRACTED HEADERS ABOVE.
-
-        var tr = table.insertRow(-1);                   // TABLE ROW.
-
-        for (var i = 0; i < col.length; i++) {
-            var th = document.createElement("th");      // TABLE HEADER.
-            th.innerHTML = col[i];
-            tr.appendChild(th);
-        }
-
-        // ADD JSON DATA TO THE TABLE AS ROWS.
-        for (var i = 0; i < myBooks.length; i++) {
-
-            tr = table.insertRow(-1);
-
-            for (var j = 0; j < col.length; j++) {
-                var tabCell = tr.insertCell(-1);
-                tabCell.innerHTML = myBooks[i][col[j]];
-            }
-        }
-
-        // FINALLY ADD THE NEWLY CREATED TABLE WITH JSON DATA TO A CONTAINER.
-        var divContainer = document.getElementById("showData");
-        divContainer.innerHTML = "";
-        divContainer.appendChild(table);
+    }
+    AddNewlyTableWithJson(table);
 }
--- src/js/code-analyzer.js
+++ src/js/code-analyzer.js
@@ -15,98 +15,128 @@ const parseCode = (codeToParse) => {
 function CreateConverter()
 {
     converter = new Map();
-    converter.set("FunctionDeclaration","function Declaration");
-    converter.set("Identifier","variable declaration");
-    converter.set("VariableDeclarator","variable declaration");
-    converter.set("AssignmentExpression", "assignment expression");
-    converter.set("WhileStatement", "while statement");
-    converter.set("IfStatement", "if statement");
-    converter.set("ElseIfStatement", "else if statement");
-    converter.set("ElseStatement","else statement");
-    converter.set("ReturnStatement","return statement");
-    converter.set("ForStatement","for statement");
+    converter.set('FunctionDeclaration','function Declaration');
+    converter.set('Identifier','variable declaration');
+    converter.set('VariableDeclarator','variable declaration');
+    converter.set('AssignmentExpression', 'assignment expression');
+    converter.set('WhileStatement', 'while statement');
+    converter.set('IfStatement', 'if statement');
+    converter.set('ElseIfStatement', 'else if statement');
+    converter.set('ElseStatement','else statement');
+    converter.set('ReturnStatement','return statement');
+    converter.set('ForStatement','for statement');
 
 }
 
+
+function CotinueSwitchCase4(expression) {
+    switch (expression.type) {
+    case('UnaryExpression'):
+        return ParseUnaryExpression(expression);
+    case('ForStatement'):
+        ParseForStatement(expression);
+        break;
+    }
+}
+
+function CotinueSwitchCase3(expression) {
+    switch (expression.type)
+    {
+    case('MemberExpression'):
+        return ParseMemberExpression(expression);
+    case('ElseIfStatement'):
+        ParseIfStatement(expression);
+        break;
+    case('ReturnStatement'):
+        ParseReturnStatement(expression);
+        break;
+    default:
+        return CotinueSwitchCase4(expression);}
+}
+
+function CotinueSwitchCase2(expression) {
+    switch (expression.type)
+    {
+    case('Literal'):
+        return ParseLiteral(expression);
+    case('BinaryExpression'):
+        return ParseBinaryExpression(expression);
+    case('WhileStatement'):
+        ParseWhileStatement(expression);
+        break;
+    case('IfStatement'):
+        ParseIfStatement(expression);
+        break;
+    default:
+        return CotinueSwitchCase3(expression);}
+}
+
+function CotinueSwitchCase(expression) {
+    switch (expression.type)
+    {
+    case('VariableDeclarator'):
+        ParseVariableDeclarator(expression);
+        break;
+    case('ExpressionStatement'):
+        ParseExpressionStatement(expression);
+        break;
+    case('AssignmentExpression'):
+        ParseAssignmentExpression(expression);
+        break;
+    case('Identifier'):
+        return ParseIdentifier(expression);
+    default:
+        return CotinueSwitchCase2(expression);}
+}
+    
+
+
+/*
+
+
+
+ */
 function ParseDataToTable(expression)
 {
     switch (expression.type)
     {
-        case ("Program"):
-            ParseProgram(expression);
-            break;
-        case ("FunctionDeclaration"):
-            ParseFunction(expression);
-            break;
-        case("BlockStatement"):
-            ParseBlockStatement(expression);
-            break;
-        case("VariableDeclaration"):
-            ParseVariableDeclaration(expression);
-            break;
-        case("VariableDeclarator"):
-            ParseVariableDeclarator(expression);
-            break;
-        case("ExpressionStatement"):
-            ParseExpressionStatement(expression);
-            break;
-        case("AssignmentExpression"):
-            ParseAssignmentExpression(expression);
-            break;
-        case("Identifier"):
-            return ParseIdentifier(expression);
-        case("Literal"):
-            return ParseLiteral(expression);
-        case("BinaryExpression"):
-            return ParseBinaryExpression(expression);
-        case("WhileStatement"):
-            ParseWhileStatement(expression);
-            break;
-        case("IfStatement"):
-            ParseIfStatement(expression);
-            break;
-        case("MemberExpression"):
-            return ParseMemberExpression(expression);
-        case("ElseIfStatement"):
-            ParseIfStatement(expression);
-            break;
-        case("ReturnStatement"):
-            ParseReturnStatement(expression);
-            break;
-        case("UnaryExpression"):
-            return ParseUnaryExpression(expression);
-        case("ForStatement"):
-            ParseForStatement(expression);
-            break;
-    }
-    return result;
+    case ('Program'):
+        ParseProgram(expression);
+        return result;
+    case ('FunctionDeclaration'):
+        ParseFunction(expression);
+        break;
+    case('BlockStatement'):
+        ParseBlockStatement(expression);
+        break;
+    case('VariableDeclaration'):
+        ParseVariableDeclaration(expression);
+        break;
+    default:
+        return CotinueSwitchCase(expression);}
+
 }
   
 
 function ParseProgram(expression)
 {
     ParseDataToTable(expression.body[0]);
+    return result;
     
 }
 
 function ParseFunction(expression)
 {
     result.push(
-    {"line":line_num,
-     "type":converter.get(expression.type),
-     "name":expression.id.name,
-     "condition":"",
-     "value":""
-    });
+        {'line':line_num,
+            'type':converter.get(expression.type), 'name':expression.id.name, 'condition':'', 'value':''
+        });
     var params = expression.params;
     for (var i = 0;i<params.length;i++)
-    result.push(
-        {"line":line_num,
-         "type":converter.get(params[i].type),
-         "name":params[i].name,
-         "condition":"",
-         "value":""
-        });
+        result.push(
+            {'line':line_num,
+                'type':converter.get(params[i].type), 'name':params[i].name, 'condition':'', 'value':''
+            });
 
     line_num++;
     ParseDataToTable(expression.body);
@@ -133,11 +163,11 @@ function ParseVariableDeclaration(expression)
 function ParseVariableDeclarator(expression)
 {
     result.push(
-        {"line":line_num,
-         "type":converter.get(expression.type),
-         "name":expression.id.name,
-         "condition":"",
-         "value":""
+        {'line':line_num,
+            'type':converter.get(expression.type),
+            'name':expression.id.name,
+            'condition':'',
+            'value':''
         });
 }
 
@@ -151,11 +181,11 @@ function ParseAssignmentExpression(expression)
     var name = ParseDataToTable(expression.left);
     var value = ParseDataToTable(expression.right);
     result.push(
-        {"line":line_num,
-         "type":converter.get(expression.type),
-         "name":name,
-         "condition":"",
-         "value":value
+        {'line':line_num,
+            'type':converter.get(expression.type),
+            'name':name,
+            'condition':'',
+            'value':value
         });
 }
 
@@ -181,71 +211,53 @@ function ParseWhileStatement(expression)
 {
     var condition = ParseDataToTable(expression.test);
     result.push(
-        {"line":line_num,
-         "type":converter.get(expression.type),
-         "name":"",
-         "condition":condition,
-         "value":""
+        {'line':line_num,
+            'type':converter.get(expression.type),
+            'name':'',
+            'condition':condition,
+            'value':''
         });
     line_num++;
-    ParseDataToTable(expression.body)
+    ParseDataToTable(expression.body);
 }
 
 function ParseIfStatement(expression)
 {
     var condition = ParseDataToTable(expression.test);
-    result.push(
-        {"line":line_num,
-         "type":converter.get(expression.type),
-         "name":"",
-         "condition":condition,
-         "value":""
-        }); 
-   
+    result.push({'line':line_num, 'type':converter.get(expression.type), 'name':'', 'condition':condition, 'value':''});
     line_num++;
     ParseDataToTable(expression.consequent);
     line_num++;
     if (expression.alternate != null)
     {
-    if (expression.alternate.type==="IfStatement")
-    {
-        expression.alternate.type= "ElseIfStatement";
-        ParseDataToTable(expression.alternate);
+        if (expression.alternate.type==='IfStatement') {
+            expression.alternate.type= 'ElseIfStatement';
+            ParseDataToTable(expression.alternate);}
+        else {
+            result.push({'line':line_num, 'type':'else statement', 'name':'', 'condition':'', 'value':''});
+            line_num++;
+            ParseDataToTable(expression.alternate);}
     }
-    else
-    {
-        result.push(
-            {"line":line_num,
-             "type":"else statement",
-             "name":"",
-             "condition":"",
-             "value":""
-            }); 
-
-        line_num++;
-        ParseDataToTable(expression.alternate);
-    }
-}
 }
 
 function ParseMemberExpression(expression)
 {
-    return ParseDataToTable(expression.object) +"[" + ParseDataToTable(expression.property) + "]";
+    return ParseDataToTable(expression.object) +'[' + ParseDataToTable(expression.property) + ']';
 }
 
 function ParseReturnStatement(expression)
 {
-    var ret
+    var ret;
     if (expression.argument!=null)
         ret = ParseDataToTable(expression.argument);
     else
-        ret = "";
+        ret = '';
     result.push(
-        {"line":line_num,
-         "type":converter.get(expression.type),
-         "name":"",
-         "condition":"",
-         "value":ret
+        {'line':line_num,
+            'type':converter.get(expression.type),
+            'name':'',
+            'condition':'',
+            'value':ret
         });
     line_num--;
 }
@@ -259,14 +271,14 @@ function ParseForStatement(expression)
 {
     var condition = ParseDataToTable(expression.test);
     result.push(
-        {"line":line_num,
-         "type":converter.get(expression.type),
-         "name":"",
-         "condition":condition,
-         "value":""
+        {'line':line_num,
+            'type':converter.get(expression.type),
+            'name':'',
+            'condition':condition,
+            'value':''
         });
     line_num++;
-    ParseDataToTable(expression.body)
+    ParseDataToTable(expression.body);
 }
 
 export {parseCode,ParseDataToTable,CreateConverter,
